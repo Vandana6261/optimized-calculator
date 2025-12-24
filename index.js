dec5e29976b97b893b7070e2136559d426dbbc07
Vandana6261 <vvandanapatidar992@gmail.com>
2025-12-24 19:10:54 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,124 @@
+// let input = document.getElementById("inputText");
+// let result = document.getElementById("result");
+// let container = document.getElementById("container");
+// let main = document.getElementById("main");
+// let enter = document.getElementById("enter");
+
+// enter.addEventListener("click", () => {
+//   let val = input.value;
+// //   let reg = /^(?=.*\d)(?!.*[+\-*^./]{2})[0-9+\-*^./]+$/g;
+//   let reg = /^(?=.*\d)(?!.*[+\-*^./]{2})[0-9+\-*^./\s]+$/;
+
+//   val = val.replaceAll(" ", "")
+//   if (!reg.test(val)) {
+//     alert("Please enter a valid regex");
+//   }
+//   console.log(val);
+//   console.log(val)
+//   let result = startCalculation(val);
+//   console.log(result)
+// });
+
+
+
+function startCalculation(arr) {
+  // 5 + 5 * 3 / 3 => 5 + 15 / 3 => 5 + 5 => 10
+  let operatorStack = [];
+  let operandStack = [];
+  for (let i = 0; i < arr.length; i++) {
+
+    // if the val is numeric value
+    if (!operatorsArr.includes(arr[i])) {
+      operandStack.push(arr[i]);
+    //   console.log(operandStack)
+    } else {
+      if (
+        operandStack.length == 0 ||
+        mp.get(arr[i]) > mp.get(operatorStack[operandStack.length - 1])
+        ) {
+        operatorStack.push(arr[i]);
+        // console.log(operatorStack)
+      } else {
+        while (
+          operatorStack.length > 0 ||
+          mp.get(arr[i]) <= mp.get(operatorStack[operatorStack.length - 1])
+        ) {
+            // console.log("46 line")
+          let operand1 = operandStack.pop();
+          let operand2 = operandStack.pop();
+          let operator = operatorStack.pop();
+          let result = calculate(operand1, operand2, operator);
+        //   console.log(result)
+          operandStack.push(result);
+        }
+        operatorStack.push(arr[i]);
+        console.log(operatorStack, "55")
+      }
+    }
+  }
+
+  while (operatorStack.length > 0) {
+    // console.log("61")
+    let operand1 = operandStack.pop();
+    let operand2 = operandStack.pop();
+    let operator = operatorStack.pop();
+    let result = calculate(operand1, operand2, operator);
+    operandStack.push(result);
+  }
+
+  return operandStack[operandStack.length - 1];
+}
+
+function calculate(operand1, operand2, operator) {
+    operand1 = Number(operand1)
+    operand2 = Number(operand2);
+
+  if (operator == "+") {
+    return operand2 + operand1;
+  } else if (operator == "-") {
+    return operand2 - operand1;
+  } else if (operator == "/") {
+    return operand2 / operand1;
+  } else if (operator == "*") {
+    return operand2 * operand1;
+  } else if (operator == "^") {
+    return Math.pow(operand2, operand1);
+  }
+}
+
+const mp = new Map([
+  ["^", 3],
+  ["*", 2],
+  ["/", 2],
+  ["+", 1],
+  ["-", 1],
+]);
+
+
+
+let operatorsArr = ["*", "/", "+", "-", "^"];
+
+let hello = "34+9/3";
+let arr = [];
+let num;
+for(let i=0; i<hello.length; i++) {
+    if(!isNaN(hello[i])) {
+        if(typeof num === "undefined") num = hello[i];
+        else num = num + hello[i];
+    } 
+    else {
+        if(typeof num !== "undefined") {
+            arr.push(num);
+            num = undefined;
+        }
+        arr.push(hello[i]);
+    }
+    if(i == hello.length - 1) if(!isNaN(hello[i])) arr.push(hello[i])
+}
+if(isNaN(arr[arr.length - 1])) arr.pop();
+// console.log(arr)
+console.log(startCalculation(arr))
+
+
+
+
