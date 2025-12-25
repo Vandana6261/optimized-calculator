d8b5e05a5f15310c39c595faedc1f38ee21b6a42
Vandana6261 <vvandanapatidar992@gmail.com>
2025-12-25 17:43:46 +0530

    structurize the code and try to write class base code

--- index.js
+++ index.js
@@ -1,116 +1,123 @@
-// let input = document.getElementById("inputText");
-// let result = document.getElementById("result");
-// let container = document.getElementById("container");
-// let main = document.getElementById("main");
-// let enter = document.getElementById("enter");
-
-// enter.addEventListener("click", () => {
-//   let val = input.value;
-// //   let reg = /^(?=.*\d)(?!.*[+\-*^./]{2})[0-9+\-*^./]+$/g;
-//   let reg = /^(?=.*\d)(?!.*[+\-*^./]{2})[0-9+\-*^./\s]+$/;
-
-//   val = val.replaceAll(" ", "")
-//   if (!reg.test(val)) {
-//     alert("Please enter a valid regex");
-//   }
-//   console.log(val);
-//   console.log(val)
-//   let result = startCalculation(val);
-//   console.log(result)
-// });
+let input = document.getElementById("inputText");
+let result = document.getElementById("result");
+let container = document.getElementById("container");
+let main = document.getElementById("main");
+let enter = document.getElementById("enter");
 
 
 
-function startCalculation(arr) {
-  let operatorStack = [];
-  let operandStack = [];
-  for (let i = 0; i < arr.length; i++) {
+enter.addEventListener("click", () => {
+  let val = input.value;
+//   let reg = /^(?=.*\d)(?!.*[+\-*^./]{2})[0-9+\-*^./]+$/g;
+  let reg = /^(?=.*\d)(?!.*[+\-*^./]{2})[0-9+\-*^./\s]+$/;
 
-    // if the val is numeric value
-    if (!operatorsArr.includes(arr[i])) {
-      operandStack.push(arr[i]);
-    } else {
-      if (
-        operandStack.length == 0 ||
-        mp.get(arr[i]) > mp.get(operatorStack[operatorStack.length - 1])
-        ) {
-        operatorStack.push(arr[i]);
+  val = val.replaceAll(" ", "")
+  if (!reg.test(val)) {
+    alert("Please enter a valid regex");
+  }
+  console.log(val);
+  let arrOfEquation = strToArr(val);
+  console.log(new Solve().startCalculation(arrOfEquation))
+
+});
+
+class Solve {
+  constructor() {
+    this.operatorsArr = ["*", "/", "+", "-", "^"];
+    this.mp = new Map([
+      ["^", 3],
+      ["*", 2],
+      ["/", 2],
+      ["+", 1],
+      ["-", 1],
+    ]);
+    this.operatorStack = [];
+    this.operandStack = [];
+    // this.startCalculation(arr);
+  }
+
+  startCalculation(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      // if the val is numeric value
+      if (!this.operatorsArr.includes(arr[i])) {
+        this.operandStack.push(arr[i]);
       } else {
-        while (
-          operatorStack.length > 0 ||
-          mp.get(arr[i]) <= mp.get(operatorStack[operatorStack.length - 1])
+        if (
+          this.operandStack.length == 0 ||
+          this.mp.get(arr[i]) > this.mp.get(this.operatorStack[this.operatorStack.length - 1])
         ) {
-          let operand1 = operandStack.pop();
-          let operand2 = operandStack.pop();
-          let operator = operatorStack.pop();
-          let result = calculate(operand1, operand2, operator);
-          operandStack.push(result);
+          this.operatorStack.push(arr[i]);
+        } else {
+          while (
+            this.operatorStack.length > 0 ||
+            this.mp.get(arr[i]) <= this.mp.get(this.operatorStack[this.operatorStack.length - 1])
+          ) {
+            let operand1 = this.operandStack.pop();
+            let operand2 = this.operandStack.pop();
+            let operator = this.operatorStack.pop();
+            let result = this.calculate(operand1, operand2, operator);
+            this.operandStack.push(result);
+          }
+          this.operatorStack.push(arr[i]);
         }
-        operatorStack.push(arr[i]);
       }
     }
+
+    while (this.operatorStack.length > 0) {
+      let operand1 = this.operandStack.pop();
+      let operand2 = this.operandStack.pop();
+      let operator = this.operatorStack.pop();
+      let result = this.calculate(operand1, operand2, operator);
+      this.operandStack.push(result);
+    }
+
+    return this.operandStack[this.operandStack.length - 1];
   }
 
-  while (operatorStack.length > 0) {
-    // console.log("61")
-    let operand1 = operandStack.pop();
-    let operand2 = operandStack.pop();
-    let operator = operatorStack.pop();
-    let result = calculate(operand1, operand2, operator);
-    operandStack.push(result);
-  }
-
-  return operandStack[operandStack.length - 1];
-}
-
-function calculate(operand1, operand2, operator) {
-    operand1 = Number(operand1)
+  calculate(operand1, operand2, operator) {
+    operand1 = Number(operand1);
     operand2 = Number(operand2);
 
-  if (operator == "+") {
-    return operand2 + operand1;
-  } else if (operator == "-") {
-    return operand2 - operand1;
-  } else if (operator == "/") {
-    return operand2 / operand1;
-  } else if (operator == "*") {
-    return operand2 * operand1;
-  } else if (operator == "^") {
-    return Math.pow(operand2, operand1);
+    if (operator == "+") {
+      return operand2 + operand1;
+    } else if (operator == "-") {
+      return operand2 - operand1;
+    } else if (operator == "/") {
+      return operand2 / operand1;
+    } else if (operator == "*") {
+      return operand2 * operand1;
+    } else if (operator == "^") {
+      return Math.pow(operand2, operand1);
+    }
   }
 }
 
-const mp = new Map([
-  ["^", 3],
-  ["*", 2],
-  ["/", 2],
-  ["+", 1],
-  ["-", 1],
-]);
 
-let operatorsArr = ["*", "/", "+", "-", "^"];
+// const solve = new Solve(arr);
+// console.log(solve.startCalculation(arr));
 
-let hello = "10-3/4";
-let arr = [];
-let num;
-for(let i=0; i<hello.length; i++) {
-    if(!isNaN(hello[i]) || hello[i] == ".") {
-        if(typeof num === "undefined") num = hello[i];
-        else num = num + hello[i];
-    } 
-    else {
-        if(typeof num !== "undefined") {
-            arr.push(num);
-            num = undefined;
+function strToArr(str) {
+    let arr = [];
+    let num;
+    for(let i=0; i<str.length; i++) {
+        if(!isNaN(str[i]) || str[i] == ".") {
+            if(typeof num === "undefined") num = str[i];
+            else num = num + str[i];
+        } else {
+            if(typeof num !== "undefined") {
+                arr.push(num);
+                num = undefined;
+            }
+            arr.push(str[i]);
         }
-        arr.push(hello[i]);
+        if(i == str.length - 1 && !isNaN(str[i])) arr.push(str[i])
     }
-    if(i == hello.length - 1) if(!isNaN(hello[i])) arr.push(hello[i])
+    return arr;
 }
-if(isNaN(arr[arr.length - 1])) arr.pop();
-console.log(arr)
-console.log(startCalculation(arr))
 
+
+
+// 10-3/4 :- [ '10', '-', '3', '/', '4' ]
 
 
 
