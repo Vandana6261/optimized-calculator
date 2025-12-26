48cd22d753768c3fd5b33952f8d301df8ffde0b2
Vandana6261 <vvandanapatidar992@gmail.com>
2025-12-26 14:24:24 +0530

    added eventListner and keyup event to listne keyboard event.

--- index.js
+++ index.js
@@ -3,38 +3,74 @@ let result = document.getElementById("result");
 let container = document.querySelector(".container");
 let main = document.getElementById("main");
 let enter = document.getElementById("enter");
-let button = document.querySelectorAll("button")
+let button = document.querySelectorAll("button");
 
-let filter_val = ["enter", "back", "clear"];
+let filter_val = ["Enter", "Backspace", "Delete"];
+let regex = /[-*+.\/0-9^]/g;
+// console.log(button)
+// console.log(container)
 
-console.log(button)
-console.log(container)
 container.addEventListener("click", (e) => {
   let clickedVal = e.target.textContent;
-  if(!filter_val.includes(clickedVal.toLowerCase())) {
-    input.value += clickedVal;
+    if(clickedVal == "Enter") {
+      Enter();
+    }
+    else if(clickedVal == "Back") {
+      Back();
+    }
+    else if(clickedVal == "Clear") {
+      Clear();
+    } else {
+      input.value += clickedVal;
+    }
+  // }
+});
+
+enter.addEventListener("click", Enter);
+
+function Enter() {
+  console.log("Enter fun called")
+    let val = input.value;
+    console.log(val);
+    //   let reg = /^(?=.*\d)(?!.*[+\-*^./]{2})[0-9+\-*^./]+$/g;
+    let reg = /^(?=.*\d)(?!.*[+\-*^./]{2})[0-9+\-*^./\s]+$/;
+
+    val = val.replaceAll(" ", "");
+    if (!reg.test(val)) {
+      alert("Please enter a valid regex");
+    }
+    console.log(val);
+    let arrOfEquation = strToArr(val);
+    console.log(new Solve().startCalculation(arrOfEquation));
+    let op = new Solve().startCalculation(arrOfEquation);
+    result.textContent = op;
+}
+
+document.addEventListener("keyup", (e) => {
+  if(filter_val.includes(e.key) || e.key.match(regex)) {
+    if(e.key === "Enter") {
+      Enter();
+    } else if(e.key === "Backspace") {
+      Back();
+    } else if(e.key === "Delete") {
+      Clear();
+    } else {
+      input.value += e.key;
+    }
   }
 })
 
+function Clear() {
+  input.value = "";
+  result.textContent = "";
+}
 
-
-
-enter.addEventListener("click", () => {
+function Back() {
   let val = input.value;
+  val = val.slice(0, -1);
   console.log(val)
-//   let reg = /^(?=.*\d)(?!.*[+\-*^./]{2})[0-9+\-*^./]+$/g;
-  let reg = /^(?=.*\d)(?!.*[+\-*^./]{2})[0-9+\-*^./\s]+$/;
-
-  val = val.replaceAll(" ", "")
-  if (!reg.test(val)) {
-    alert("Please enter a valid regex");
-  }
-  console.log(val);
-  let arrOfEquation = strToArr(val);
-  console.log(new Solve().startCalculation(arrOfEquation))
-  console.log(arrOfEquation)
-
-});
+  input.value = val;
+}
 
 class Solve {
   constructor() {
@@ -59,13 +95,15 @@ class Solve {
       } else {
         if (
           this.operandStack.length == 0 ||
-          this.mp.get(arr[i]) > this.mp.get(this.operatorStack[this.operatorStack.length - 1])
+          this.mp.get(arr[i]) >
+            this.mp.get(this.operatorStack[this.operatorStack.length - 1])
         ) {
           this.operatorStack.push(arr[i]);
         } else {
           while (
             this.operatorStack.length > 0 ||
-            this.mp.get(arr[i]) <= this.mp.get(this.operatorStack[this.operatorStack.length - 1])
+            this.mp.get(arr[i]) <=
+              this.mp.get(this.operatorStack[this.operatorStack.length - 1])
           ) {
             let operand1 = this.operandStack.pop();
             let operand2 = this.operandStack.pop();
@@ -107,32 +145,26 @@ class Solve {
   }
 }
 
-
 // const solve = new Solve(arr);
 // console.log(solve.startCalculation(arr));
 
 function strToArr(str) {
-    let arr = [];
-    let num;
-    for(let i=0; i<str.length; i++) {
-        if(!isNaN(str[i]) || str[i] == ".") {
-            if(typeof num === "undefined") num = str[i];
-            else num = num + str[i];
-        } else {
-            if(typeof num !== "undefined") {
-                arr.push(num);
-                num = undefined;
-            }
-            arr.push(str[i]);
-        }
-        if(i == str.length - 1 && !isNaN(str[i])) arr.push(num)
+  let arr = [];
+  let num;
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(str[i]) || str[i] == ".") {
+      if (typeof num === "undefined") num = str[i];
+      else num = num + str[i];
+    } else {
+      if (typeof num !== "undefined") {
+        arr.push(num);
+        num = undefined;
       }
-    return arr;
+      arr.push(str[i]);
+    }
+    if (i == str.length - 1 && !isNaN(str[i])) arr.push(num);
+  }
+  return arr;
 }
 
-
-
 // 10-3/4 :- [ '10', '-', '3', '/', '4' ]
-
-
-
